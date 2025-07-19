69b0864a936f0a84d690283286676a0302a2936d
YeCanming <ycm24@mails.tsinghua.edu.cn>
2025-07-19 17:25:32 +0800

    fix: dtypes

--- code/src/stage11_add_stock_info.py
+++ code/src/stage11_add_stock_info.py
@@ -4,17 +4,12 @@ from auto_config import project_dir
 import pandas as pd
 import joblib
 all_stock_info_df = joblib.load(project_dir / "data/additional/all_stock_info_df.joblib")
+# 将 上市时间 列转换为 datetime 类型
+all_stock_info_df['上市时间'] = pd.to_datetime(all_stock_info_df['上市时间'], format='%Y%m%d')
+all_stock_info_df = all_stock_info_df.convert_dtypes()
 all_stock_info_df
+
 #%%
-should_not_use = [
-    "最新", # 泄露未来信息
-    "总市值", # 当前股价决定，泄露了当前股价
-    "流通市值",
-]
-# %%
-df158 = pd.read_pickle(project_dir / "temp/qlib_alpha158_ranked.pkl")
-df360 = pd.read_pickle(project_dir / "temp/qlib_alpha360_ranked.pkl")
-# %%
 all_stock_info_df = all_stock_info_df.rename(columns={
     "股票代码": "item_id",
     "股票简称": "Name",
@@ -27,6 +22,19 @@ all_stock_info_df = all_stock_info_df.rename(columns={
 all_stock_info_df['item_id'] = all_stock_info_df['item_id'].astype(int)
 # 
 all_stock_info_df = all_stock_info_df.set_index('item_id')
+
+#%%
+should_not_use = [
+    "最新", # 泄露未来信息
+    "总市值", # 当前股价决定，泄露了当前股价
+    "流通市值",
+    "prompt"
+]
+# %%
+df158 = pd.read_pickle(project_dir / "temp/qlib_alpha158_ranked.pkl")
+df360 = pd.read_pickle(project_dir / "temp/qlib_alpha360_ranked.pkl")
+# %%
+
 
 df158 = df158.reset_index()      # 得到 'timestamp'、'item_id' 两列
 df158 = df158.merge(all_stock_info_df,
