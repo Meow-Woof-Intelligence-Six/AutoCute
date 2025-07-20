6862929016d97f3c4e8e3bccde7a607ee06da437
YeCanming <ycm24@mails.tsinghua.edu.cn>
2025-07-20 23:08:31 +0800

    feat: simple price

--- code/src/stage45_train_price_agts_stats.py
+++ code/src/stage45_train_price_agts_stats.py
@@ -1,32 +1,5 @@
 # %%
-# from autogluon.timeseries.models.local.statsforecast
-from auto_config import project_dir, valid_csv_path
-import pandas as pd
-
-df = pd.read_csv(valid_csv_path)
-df
-# %%
-TARGET_LABEL = "收盘"  # 真实的目标标签
-df = df[["股票代码", "日期", TARGET_LABEL]]
-df = df.rename(
-    columns={"股票代码": "item_id", "日期": "timestamp", TARGET_LABEL: TARGET_LABEL}
-)
-df["timestamp"] = pd.to_datetime(df["timestamp"])
-df["item_id"] = df["item_id"].astype(int)
-# df.dtypes
-# df
-# %%
-from autogluon.timeseries import TimeSeriesDataFrame
-
-data = TimeSeriesDataFrame.from_data_frame(
-    df,
-    id_column="item_id",
-    timestamp_column="timestamp",
-)
-# 非交易日的处理逻辑
-data = data.convert_frequency(freq="D").fill_missing_values("interpolate")
-data
-# %%
+from stage35_simple_price_data import TARGET_LABEL, data, project_dir
 # metric_name = "MASE"
 metric_name = "RMSSE"
 prediction_length = 3
